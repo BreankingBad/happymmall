d8bc9405fbe68e6198fa30086a9ca7a3d4da84a1
mxm <fjp790308448@gmail.com>
2017-12-06 22:02:37 +0800

    多个entry的配置

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,2 +1,9 @@
-cats = require('./cats.js');
-console.log(cats);
+/*
+* @Author: mxm
+* @Date:   2017-12-05 23:20:59
+* @Last Modified by:   mxm
+* @Last Modified time: 2017-12-06 21:57:03
+*/
+'use strict';
+
+console.log('hello index');
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
+/*
+* @Author: mxm
+* @Date:   2017-12-06 21:18:51
+* @Last Modified by:   mxm
+* @Last Modified time: 2017-12-06 22:00:03
+*/
  const path = require('path');
 
  module.exports = {
-     entry: './src/page/index/index.js',
+     entry: {
+     	'index' : ['./src/page/index/index.js'],
+     	'login' : ['./src/page/login/index.js'],
+ 	 },
      output: {
          path: path.resolve(__dirname, 'dist'),
-         filename: 'app.bundle.js'
+         filename: 'js/[name].js'
      }
  };
