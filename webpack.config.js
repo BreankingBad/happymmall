96b602818d3da15e1f16586f5b876536ab2a157d
mxm <fjp790308448@gmail.com>
2017-12-08 23:15:14 +0800

    增加CommonChunks插件提取代码中的公共模块，然后将公共模块打包到一个独立的文件中去

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,10 @@
 * @Author: mxm
 * @Date:   2017-12-06 21:18:51
 * @Last Modified by:   mxm
-* @Last Modified time: 2017-12-06 22:38:45
+* @Last Modified time: 2017-12-08 23:10:58
 */
+
+ var webpack = require('webpack');
  const path = require('path');
 
  module.exports = {
@@ -17,5 +19,11 @@
      },
      externals: {
      	'jquery' : 'window.jQuery'
-     }
+     },
+     plugins: [
+        new  webpack.optimize.CommonsChunkPlugin({
+        	name: 'commons',
+        	filename: 'js/base.js'
+        })
+    ]
  };
