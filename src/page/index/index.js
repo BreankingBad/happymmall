5cc82efd2f0aaa8228c864d36b4c6e53d602319d
mxm <fjp790308448@gmail.com>
2017-12-08 23:51:20 +0800

    webpack 分离css单独打包

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,9 +2,11 @@
 * @Author: mxm
 * @Date:   2017-12-05 23:20:59
 * @Last Modified by:   mxm
-* @Last Modified time: 2017-12-06 22:30:46
+* @Last Modified time: 2017-12-08 23:41:37
 */
 'use strict';
+
+require('./index.css');
 
 console.log('hello index');
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,12 @@
 * @Author: mxm
 * @Date:   2017-12-06 21:18:51
 * @Last Modified by:   mxm
-* @Last Modified time: 2017-12-08 23:23:09
+* @Last Modified time: 2017-12-08 23:49:22
 */
 
- var webpack = require('webpack');
+ var webpack 			= require('webpack');
+ var ExtractTextPlugin 	= require("extract-text-webpack-plugin");
+
  const path = require('path');
 
  module.exports = {
@@ -21,10 +23,19 @@
      externals: {
      	'jquery' : 'window.jQuery'
      },
+     module: {
+		loaders: [
+				{
+				     test: /\.css$/,
+				     loader:  ExtractTextPlugin.extract("style-loader","css-loader")
+				 }
+		     ]
+     },
      plugins: [
         new  webpack.optimize.CommonsChunkPlugin({
         	name: 'common',
         	filename: 'js/base.js'
-        })
+        }),
+        new ExtractTextPlugin("css/[name].css"),
     ]
  };
