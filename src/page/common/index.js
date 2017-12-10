9f4ca8699ec59716f62a44af445c464cf2e09b88
mxm <fjp790308448@gmail.com>
2017-12-11 00:08:51 +0800

    底部footer 实现

--- src/page/common/index.js
+++ src/page/common/index.js
@@ -2,9 +2,10 @@
 * @Author: mxm
 * @Date:   2017-12-08 23:20:55
 * @Last Modified by:   mxm
-* @Last Modified time: 2017-12-10 19:49:14
+* @Last Modified time: 2017-12-11 00:02:05
 */
 console.log('i am global');
 
 require('./layout.css');
 require('node_modules_dir/font-awesome/css/font-awesome.min.css');
+require('./footer/index.css');
