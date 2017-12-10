5eb94454caf9d511429c8de85fe2e6078dec9ebe
mxm <fjp790308448@gmail.com>
2017-12-10 19:41:08 +0800

    全局通用css样式

--- src/page/common/index.js
+++ src/page/common/index.js
@@ -2,6 +2,8 @@
 * @Author: mxm
 * @Date:   2017-12-08 23:20:55
 * @Last Modified by:   mxm
-* @Last Modified time: 2017-12-08 23:21:49
+* @Last Modified time: 2017-12-10 11:46:33
 */
 console.log('i am global');
+
+require('./layout.css');
