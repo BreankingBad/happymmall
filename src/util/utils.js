3de5115e8ee3b8e016685f144ff3fafe04f6a711
mxm <fjp790308448@gmail.com>
2017-12-10 11:43:03 +0800

    工具类增加手机号验证／邮箱验证／返回主页的方法

--- src/util/utils.js
+++ src/util/utils.js
@@ -2,7 +2,7 @@
 * @Author: mxm
 * @Date:   2017-12-09 21:47:18
 * @Last Modified by:   mxm
-* @Last Modified time: 2017-12-10 11:18:02
+* @Last Modified time: 2017-12-10 11:42:18
 */
 
 var Hogan = require('hogan');
@@ -56,9 +56,37 @@ var utils = {
 		result = template.render(data);
 		return result;
 	},
+	// 成功提示
+	successTips : function(msg) {
+		alert(msg || '操作成功！');
+	},	
+	// 错误提示
+	errorTips : function(msg) {
+		alert(msg || '哪里不对了～');
+	},
+	// 字段的验证，支持非空、手机、邮箱的判断
+	validate : function(value, type){
+		var value = $.trim(value);
+		// 非空验证
+		if('require' === type) {
+			return !!value;
+		}
+		// 手机号验证
+		if('phone' === type){
+			return /^1\d{10}$/.test(value);
+		}
+		// 邮箱格式验证
+		if('email' === type){
+			return /^(\w)+(\.\w+)*@(\w)+((\.\w{2,3}){1,3})$/.test(value);
+		}
+	},
 	// 统一登录处理
 	doLogin : function(){
 		window.location.href = './login.html?redirect=' + encodeURIComponent(window.location.href);
+	},
+	// 返回主页
+	goHome : function() {
+		window.location.href = './index.html';
 	}
 };
 
