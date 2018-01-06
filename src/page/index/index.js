2fd5d3e395c69fceae0ee3cd64fb7ad7e8ee34df
mxm <fjp790308448@gmail.com>
2018-01-06 10:36:30 +0800

    首页商品分类列表显示

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,18 +2,17 @@
 * @Author: mxm
 * @Date:   2017-12-05 23:20:59
 * @Last Modified by:   mxm
-* @Last Modified time: 2017-12-13 22:00:19
+* @Last Modified time: 2018-01-06 10:09:29
 */
 'use strict';
 
+require('./index.css');
 require('pageDir/common/nav/index.js');
 require('pageDir/common/header/index.js');
 var navSide = require('pageDir/common/nav-side/index.js');
 var utils = require('utilDir/utils.js');
 
-navSide.init({
-	name : 'pass-update'
-});
+
 
 
 // console.log('hello index');
