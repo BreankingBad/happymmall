87503e10c5c9555db17b6470aa443684a374cedd
mxm <fjp790308448@gmail.com>
2018-01-06 17:55:29 +0800

    6.4 引用unslider.js实现首页banner

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,37 +2,29 @@
 * @Author: mxm
 * @Date:   2017-12-05 23:20:59
 * @Last Modified by:   mxm
-* @Last Modified time: 2018-01-06 10:09:29
+* @Last Modified time: 2018-01-06 17:52:58
 */
 'use strict';
 
 require('./index.css');
 require('pageDir/common/nav/index.js');
 require('pageDir/common/header/index.js');
+require('utilDir/slider/index.js');
 var navSide = require('pageDir/common/nav-side/index.js');
+var templateBanner = require('./index.string');
 var utils = require('utilDir/utils.js');
 
 
+$(function() {
 
+	var bannerHtml = utils.renderHtml(templateBanner);
+	$('.banner-con').html(bannerHtml);
+    var $slider = $('.banner').unslider({
+    	"dots" : true
+    });
 
-// console.log('hello index');
-
-// $('body').html('hello ind');
-
-// utils.request({
-// 	url : '/product/list.do?keyword=1',
-// 	success : function(res){
-// 		console.log(res);
-// 	},
-// 	error : function(err){
-// 		console.log(err);
-// 	}
-// });
-
-// console.log(utils.getUrlParam('test'));
-
-// var html = '<div>{{data}}</div>';
-// var data = {
-// 	data : 'test'
-// }
-// console.log(utils.renderHtml(html,data));
+    $('.banner-con .banner-arrow').click(function(){
+    	var forward = $(this).hasClass('prev') ? 'prev' : 'next';
+    	$slider.data('unslider')[forward]();  
+    });
+});
---
+++ src/util/slider/index.js
@@ -0,0 +1,10 @@
+/*
+* @Author: mxm
+* @Date:   2018-01-06 16:44:33
+* @Last Modified by:   mxm
+* @Last Modified time: 2018-01-06 16:46:34
+*/
+
+
+require('./index.css');
+require('./unslider.js');
